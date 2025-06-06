13c80d29813fae32b15bc5b6e7e9fec076580923
cory <cory@Corys-Mac-Mini-1.local>
2025-06-06 20:41:31 +0800

    Pass required props to DashboardFilters

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+
+import { useState } from 'react';
 import { DashboardFilters } from '@/components/DashboardFilters';
 import TopMetrics from '@/components/TopMetrics';
+import { DateRange } from 'react-day-picker';
 
 export default function Home() {
+  const [dateRange, setDateRange] = useState<DateRange | undefined>(undefined);
+
   return (
     <main className="p-6">
-      <DashboardFilters />
+      <DashboardFilters dateRange={dateRange} setDateRange={setDateRange} />
       <TopMetrics />
     </main>
   );
